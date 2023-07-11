fb1aa0a2db2d8f2ee479b5ee8b01b6bc951cbab0
NamasakaLennox <namasakalennox254@gmail.com>
2023-07-11 18:57:54 +0300

    recreated the instance with a dictionary representation

--- len_test/models/base_model.py
+++ len_test/models/base_model.py
@@ -8,13 +8,22 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Constructor that initializes the instance attributes
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            my_format = '%Y-%m-%dT%H:%M:%S.%f'
+            for key, value in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, my_format)
+                if key != "__class__":
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
